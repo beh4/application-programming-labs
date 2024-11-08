086d5fc872e3238d6b32331a82567806b226cb84
beh4 <rmansyr06@gmail.com>
2024-11-08 19:58:46 +0400

    Corrections

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 
-def read_image(file_path) -> np.ndarray:
+
+def read_image(file_path: str) -> np.ndarray:
     """
     Считывает изображение из файла.
 
@@ -13,7 +14,8 @@ def read_image(file_path) -> np.ndarray:
     image = cv2.imread(file_path)
     return image
 
-def get_image_size(image) -> tuple:
+
+def get_image_size(image: np.ndarray) -> tuple:
     """
     Получает размер изображения.
 
@@ -23,23 +25,25 @@ def get_image_size(image) -> tuple:
     height, width = image.shape[:2]
     return height, width
 
-def plot_histogram(image) -> None:
+
+def plot_histogram(image: np.ndarray) -> None:
     """
     Строит гистограмму для цветного изображения.
 
     :param image: Изображение как массив NumPy
     """
     color = ('b', 'g', 'r')
-    for i, col in enumerate(color):
+    for i,col in enumerate(color):
         hist = cv2.calcHist([image], [i], None, [256], [0, 256])
-        plt.plot(hist, color=col)
+        plt.plot(hist, color = col)
         plt.xlim([0, 256])
     plt.title('Image histogram')
     plt.xlabel('Intensity')
     plt.ylabel('Frequency')
     plt.show()
 
-def crop_image(image, size) -> np.ndarray:
+
+def crop_image(image: np.ndarray, size: tuple[int, int]) -> np.ndarray:
     """
     Обрезает изображение до заданных размеров, начиная от левого верхнего угла.
 
@@ -47,10 +51,14 @@ def crop_image(image, size) -> np.ndarray:
     :param size: Кортеж (высота, ширина) для обрезки
     :return: Обрезанное изображение
     """
+    main_height, main_width = get_image_size(image)
     height, width = size
+    if main_height < height or main_width < width:
+        image = cv2.resize(image, (max(width, main_width), max(height, main_height)))
     return image[0:height, 0:width]
 
-def display_image(image, title='Image') -> None:
+
+def display_image(image: np.ndarray, title: str = 'Image') -> None:
     """
     Отображает изображение с заданным заголовком.
 
@@ -60,7 +68,8 @@ def display_image(image, title='Image') -> None:
     cv2.imshow(title, image)
     cv2.waitKey(0)
 
-def save_image(image) -> None:
+
+def save_image(image: np.ndarray) -> None:
     """
     Сохраняет изображение в файл.
 
@@ -68,7 +77,8 @@ def save_image(image) -> None:
     """
     cv2.imwrite('edited6.png', image)
 
-def main(input_path, crop_height, crop_width):
+
+def main(input_path: str, crop_height: int, crop_width: int):
     """
     Основная функция для выполнения всех операций.
 
@@ -91,8 +101,6 @@ def main(input_path, crop_height, crop_width):
 
         # Если указаны размеры для обрезки, выполняем обрезку
         if crop_height and crop_width:
-            if crop_height > height or crop_width > width:
-                raise ValueError(f"Crop dimensions exceed image dimensions.")
             image = crop_image(image, (crop_height, crop_width))
 
         # Отображаем изображение
@@ -103,6 +111,7 @@ def main(input_path, crop_height, crop_width):
 
     except Exception as e:
         print(f"An error has occurred: {e}")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Обработка изображения.')
